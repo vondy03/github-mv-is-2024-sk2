7c15fab2222c186a89a69d51bdcbbf816d626e93
Matyáš Vondráček <matyas.vondracek@tul.cz>
2024-10-10 13:54:50 +0200

    #1 vstup hodnot a jejich výpis

--- IS-Projekty/program001-vypis-rady/Program.cs
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 class Program {
     static void Main() {
@@ -13,7 +14,38 @@ class Program {
         Console.WriteLine ("********************************\n");
         Console.WriteLine ();
 
-        //Vstup od uživatele -TO DO
+        //Vstup od uživatele -špatná varianta
+        //Console.WriteLine("Zadejte první číslo řady:");
+        //int first = int.Parse(Console.ReadLine());
+
+
+        //Vstup od uživatele -správná varianta
+        Console.WriteLine("Zadejte první číslo řady (celé číslo):");
+        int first;
+        while (!int.TryParse(Console.ReadLine(),out first)){
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo):");
+        }
+
+        Console.WriteLine("Zadejte poslední číslo řady (celé číslo):");
+        int last;
+        while (!int.TryParse(Console.ReadLine(),out last)){
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu poslední číslo řady (celé číslo):");
+        }
+
+        Console.WriteLine("Zadejte velikost kroku mezi čísly řady (celé číslo):");
+        int step;
+        while (!int.TryParse(Console.ReadLine(),out step)){
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu velikost kroku mezi čísly řady (celé číslo):");
+        }
+
+        //Výpis uživatelského výstupu
+        Console.WriteLine();
+        Console.WriteLine("=====================================");
+        Console.WriteLine("První číslo řady: {0}", first);
+        Console.WriteLine("Poslední číslo řady: {0}", last);
+        Console.WriteLine("Velikost kroku mezi čísly řady: {0}", step);
+
+
 
         //Logika pro výpis řady - TO DO
 
